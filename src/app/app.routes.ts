af3267d00f38cfc2d51e62b83fc8112116b8b75e
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-05-09 21:14:46 +0200

    add all necessary setup to begin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandigPage } from './pages/landig-page/landig-page';
+import { MainPage } from './pages/main-page/main-page';
+import { LoginPage } from './pages/login-page/login-page';
+import { RegisterPage } from './pages/register-page/register-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Library', component: LandigPage },
+  { path: 'main', title: 'Main', component: MainPage },
+  { path: 'login', title: 'Login', component: LoginPage },
+  { path: 'register', title: 'Register', component: RegisterPage },
+  { path: '**', redirectTo: '' },
+];
