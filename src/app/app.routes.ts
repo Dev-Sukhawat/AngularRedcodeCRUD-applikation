89322ce3e96db8c3a85676d2c0e78868bd7e3ba1
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-05-13 19:20:34 +0200

    edit route and add new route like Books, Quotes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandigPage } from './pages/landig-page/landig-page';
-import { MainPage } from './pages/main-page/main-page';
+import { Books } from './pages/books/books';
+import { Quotes } from './pages/quotes/quotes';
 import { LoginPage } from './pages/login-page/login-page';
 import { RegisterPage } from './pages/register-page/register-page';
 
 export const routes: Routes = [
   { path: '', title: 'Library', component: LandigPage },
-  { path: 'main', title: 'Main', component: MainPage },
+  { path: 'books', title: 'Books', component: Books },
+  { path: 'quotes', title: 'Quotes', component: Quotes },
   { path: 'login', title: 'Login', component: LoginPage },
   { path: 'register', title: 'Register', component: RegisterPage },
   { path: '**', redirectTo: '' },
