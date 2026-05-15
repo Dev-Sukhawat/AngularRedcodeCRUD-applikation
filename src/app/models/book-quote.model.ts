745da6fc4ca390067e21c722f6f510f6d6258383
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-05-15 12:17:07 +0200

    feat: add Book and Quote interfaces for data modeling

---
+++ src/app/models/book-quote.model.ts
@@ -0,0 +1,13 @@
+export interface Book {
+  id: string;
+  title: string;
+  author: string;
+  created_at: string;
+}
+
+export interface Quote {
+  id: string;
+  title: string;
+  text: string;
+  created_at: string;
+}
